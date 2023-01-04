6c25ddab2f5daaa64e6607c353399cc709c53abb
zevin02 <1833195090@qq.com>
2023-01-04 23:29:27 +0800

    set

--- file.py
+++ file.py
@@ -5,26 +5,27 @@ print(f)
 f.close()
 #打开文件的
 f=open("raw.txt",'w')
-f.write("hello")
-f.write('111')
+a=-1
+f.write(f"hello {a}")
+# f.write('111')
 print(f)
 f.close()
 
-f=open("raw.txt",'r',encoding='utf8')
-#read(n):n 是要读取多少个字符
-#没有参数就是读取所有的字符
-result=f.read()
-print(result)
-lists=f.readlines()
-print(lists)
-#按行读取文件的所有元素
+# f=open("raw.txt",'r',encoding='utf8')
+# #read(n):n 是要读取多少个字符
+# #没有参数就是读取所有的字符
+# result=f.read()
+# print(result)
+# lists=f.readlines()
+# print(lists)
+# #按行读取文件的所有元素
 
-for line in f:
-    print(line)
+# for line in f:
+#     print(line)
 
 
-#上下文管理器
-def func():
-    #with会自动管理关闭文件f
-    with open("raw.txt","r") as f:
-        
+# #上下文管理器
+# def func():
+#     #with会自动管理关闭文件f
+#     with open("raw.txt","r") as f:
+#         pass
